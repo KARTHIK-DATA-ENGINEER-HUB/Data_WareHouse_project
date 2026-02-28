5909a98afa60774652154f0cce21515624453a4d
KARTHIK-DATA-ENGINEER-HUB <venkatakarthik725@gmail.com>
2026-02-28 18:29:29 +0530

    Create Datawarehouse database and schemas
    This script creates a database named 'Datawarehouse' and schemas 'bronze', 'silver', and 'gold'. It also includes a warning about data loss if the database already exists.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+-----------------------------------CREATE DATABASE AND SCHEMAS------------------------------
+/* This script creats a database by checking if it exists or not if database exists it will drop the database and create a 
+new one.It also creates the schemas bronze,silver and gold with in this database Datawarehousee*/
 
+/***WARNING:This script will delete all the data inside the database.please ensure that you have a backup of the data in the database before 
+using the script*/
+----------------------------------CREATE DATABASE Datawarehouse------------------------------
+USE MASTER;
+IF EXISTS(SELECT 1 FROM sys.databases  WHERE NAME='Datawarehouse')
+BEGIN
+	DROP DATABSE Datawarehouse
+END
+GO
+CREATE DATABASE Datawarehouse;
+USE Datawarehouse
+GO
+-------------------------------------CREATING SCHEMAS-----------------------------------------
+CREATE SCHEMA bronze
+GO
+CREATE SCHEMA silver
+GO
+CREATE SCHEMA gold
