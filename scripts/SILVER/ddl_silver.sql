9430a85ce94a851ddd54da458814f7ab5af3439f
KARTHIK-DATA-ENGINEER-HUB <venkatakarthik725@gmail.com>
2026-03-01 16:58:48 +0530

    Add documentation for silver schema DDL script
    Added a comment block explaining the purpose of the DDL script.

--- scripts/SILVER/ddl_silver.sql
+++ scripts/SILVER/ddl_silver.sql
@@ -1,4 +1,11 @@
-
+/*
+===================================
+DDL SCRIPT : TO CREATE SILVER TABLES
+=====================================
+Script Purpose: 
+  This script creates the tables in the silver schema,dropoping the existing tables if they already exist.
+  Run this script if you want to redefine the structure of tables in the silver layer.
+=======================================*/
 IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
 	DROP TABLE  silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
